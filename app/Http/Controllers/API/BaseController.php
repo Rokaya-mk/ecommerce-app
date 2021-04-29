f901a6cccff17aec8031018be4dec26aac0f3ede
AlwaleedAlroomi <alwaleed101az@gmail.com>
2021-04-30 01:51:31 +0300

    50% finished of users table requirments

---
+++ app/Http/Controllers/API/BaseController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller as Controller;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    public function SendResponse($result, $message){
+        $respones = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message,
+        ];
+        return response()->json($respones, 200);
+    }
+
+    public function SendError($error, $errorMessage = [], $code = 404){
+        $respones = [
+            'success' => false,
+            'message' => $error,
+        ];
+        return response()->json($respones, $code);
+    }
+}
