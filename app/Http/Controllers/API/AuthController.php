1190eb7e0aa8189b4ada99eaac31066853d3f365
AlwaleedAlroomi <alwaleed101az@gmail.com>
2021-05-12 03:34:18 +0300

    Users 95% done

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -31,34 +31,49 @@ class AuthController extends BaseController
         $input = $request->all();
         $input['password'] = Hash::make($input['password']);
         $user = User::create($input);
-	//send the verify code
-	$token = Str::random(5);
-	try{
-		DB::table('users')->where('email', $request['email'])->update([
-			'is_verify' => $token,
-		]);
-		$email = $request['email'];
-		Mail::send('Mails.verify', ['token' => $token], function ($message) use ($email) {
-			$message->to($email);
-			$message->subject('Verify your email');
-		});
-	}catch(\Exception $exception){
-		return $this->SendError($exception->getMessage(), 400);
-	}
-	$success['name'] = $user->name;
-	$success['is_Admin'] = $user->is_Admin;
+	    //send the verify code
+	    $token = Str::random(5);
+	    try{
+		    DB::table('users')->where('email', $request['email'])->update([
+			    'is_verify' => $token,
+		    ]);
+		    $email = $request['email'];
+		    Mail::send('Mails.verify', ['token' => $token], function ($message) use ($email) {
+			    $message->to($email);
+			    $message->subject('Verify your email');
+		    });
+	    }catch(\Exception $exception){
+		    return $this->SendError($exception->getMessage(), 400);
+	    }
+	    $success['name'] = $user->name;
+	    $success['is_Admin'] = $user->is_Admin;
         $success['token'] = $user->createToken('customer')->accessToken;
         return $this->SendResponse($success, 'Registered successfully');
     }
 
     public function login(Request $request){
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            if ($user->is_Admin == 1) {
+                $token = Str::random(5);
+                DB::table('users')->where('email', $request['email'])->update([
+                    'is_verify' => $token,
+                ]);
+                $email = $request['email'];
+                Mail::send('Mails.verify', ['token' => $token], function ($message) use ($email) {
+                    $message->to($email);
+                    $message->subject('Verify your email');
+                });
+                $this->SendResponse('verify your email', 200);
+            }
+        }
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
-		$user = Auth::user();
-		if($user->is_verify != 1){
-			return $this->SendError('Verify your email', 400);
-		}
+		    $user = Auth::user();
+		    if($user->is_verify != 1){
+			    return $this->SendError('Verify your email', 400);
+		    }
             $success['name'] = $user->name;
-	    $success['is_Admin'] = $user->is_Admin;
+	        $success['is_Admin'] = $user->is_Admin;
             $success['token'] = $user->createToken('customer')->accessToken;
             return $this->SendResponse($success, 'Customer logged in successfully');
         }else{
@@ -67,13 +82,16 @@ class AuthController extends BaseController
     }
    //To change the user informations
     public function updateUserInformation(Request $request){
-	$user = Auth::user();
+	    $user = Auth::user();
         $validator = Validator::make($request->all(),[
             'name' => 'required',
             'shippingAddress' => 'required',
         ]);
+        if($validator->fails()){
+            return $this->SendError('Validate Error', $validator->errors());
+        }
 
-	$user->name = $request->name;
+	    $user->name = $request->name;
         $user->shippingAddress = $request->shippingAddress;
         $user->save();
         return $this->SendResponse('Information Updated Successfully', 200);
@@ -92,9 +110,12 @@ class AuthController extends BaseController
     //forgot password
     public function forgot(Request $request){
         $email = $request['email'];
-        $validate = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(),[
             'email' => 'required | email',
         ]);
+        if($validator->fails()){
+            return $this->SendError('Validate Error', $validator->errors());
+        }
         //To check if the email is true or not
         if(User::where('email', $email)->doesntExist()){
             return $this->SendError('Email is not exist', 404);
@@ -102,6 +123,19 @@ class AuthController extends BaseController
         //Generate the forgot code
         $token = Str::random(4);
         try {
+            if (DB::table('password_resets')->where('email', $request['email'])->first()) {
+                DB::table('password_resets')->where('email', $request['email'])->update([
+                    'token' => $token,
+
+                ]);
+                $email = $request['email'];
+                Mail::send('Mails.forgot', ['token' => $token], function ($message) use ($email){
+                    $message->to($email);
+                    $message->subject('Reset your password');
+                    $message->priority(1);
+                });
+                return $this->SendResponse('check your email', 200);
+            }
             //To add the token to th db to check it later
             DB::table('password_resets')->insert([
                 'email' => $email,
@@ -119,29 +153,40 @@ class AuthController extends BaseController
         }
     }
     public function passwordReset(Request $request){
-        $validate = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|email',
             'token' => 'required',
             'password' => 'required',
             'c_password' => 'required |same:password',
         ]);
-
-        $token = $request['token'];
-        if (!$passwordResets = DB::table('password_resets')->where('token', $token)->first()) {
+        if($validator->fails()){
+            return $this->SendError('Validate Error', $validator->errors());
+        }
+        if (User::where('email', $request['email'])->doesntExist()) {
+            $this->SendError('Invalid Email', 404);
+        }
+        if (!DB::table('password_resets')->where('email', $request['email'])->first()) {
+            return $this->SendError('Invalid email', 404);
+        }
+        if (!DB::table('password_resets')->where('token', $request['token'])->first()) {
             return $this->SendError('Invalid token', 404);
         }
-        if (!$user = User::where('email', $passwordResets->email)->first()) {
-            return $this->SendError('User does not exist', 404);
-        }
+        $user = User::where('email', $request['email'])->first();
         $user->password = Hash::make($request['password']);
+        DB::table('password_resets')->where('email', $request['email'])->where('token', $request['token'])->delete();
         $user->save();
         return $this->SendResponse('User password changed successfully', 200);
     }
 
     public function emailVerify(Request $request){
-	    $validate = Validator::make($request->all(), [
+	    $validator = Validator::make($request->all(), [
 		    'email' => 'required | email',
 		    'token' => 'required',
 	    ]);
+        if($validator->fails()){
+            return $this->SendError('Validate Error', $validator->errors());
+        }
+
 	    $user = User::where('email', $request['email'])->first();
 	    $token = $request['token'];
 	    if($user->is_verify == $token){
@@ -150,6 +195,29 @@ class AuthController extends BaseController
 		    $user->save();
 		    return $this->SendResponse('Email verified successfully', 200);
 	    }
+
+        if ($user->is_verify == 1) {
+            return $this->SendError('Email is already Verified', 400);
+        }
 	    return $this->SendError('Wrong token', 404);
+    }
+
+    public function resendCode(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+        ]);
+        $email = $request['email'];
+        $token = Str::random(4);
+        DB::table('password_resets')->where('email', $request['email'])->update([
+            'token' => $token,
+
+        ]);
+        $email = $request['email'];
+        Mail::send('Mails.forgot', ['token' => $token], function ($message) use ($email){
+            $message->to($email);
+            $message->subject('Reset your password');
+            $message->priority(1);
+        });
+        return $this->SendResponse('check your email', 200);
     }
 }
