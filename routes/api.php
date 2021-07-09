2c1367b0af856e16ab089c1ab733bcdc0edd248e
Alwaleed Alroomi <alwaleed101az@gmail.com>
2021-07-09 03:27:01 +0300

    change email option

--- routes/api.php
+++ routes/api.php
@@ -120,3 +120,4 @@ Route::delete('delete/review/{id}', 'API\ReviewController@deleteReview')->middle
 
 Route::post('sendEmail', 'API\SendEmailController@sendEmailForAllUsers')->middleware('auth:api');
 Route::post('send/Email/for/users', 'API\SendEmailController@sendEmailForSpecificUsers')->middleware('auth:api');
+Route::post('change/email/option/{id}', 'API\SendEmailController@changeAcceptEmailOption')->middleware('auth:api');
